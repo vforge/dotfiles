a98ad5b1d0b9c89600f4f3f5313de5bc1f46d74b
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2020-07-16 12:46:25 -0700

    update_1594928785

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH=./bin:$HOME/bin:$HOME/.yarn/bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH
+export PATH=./bin:$HOME/bin:$HOME/.yarn/bin:/usr/local/bin:/usr/local/sbin:$HOME/.config/yarn/global/node_modules/.bin:$DOTFILES/bin:$PATH
 export MANPATH=/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH
