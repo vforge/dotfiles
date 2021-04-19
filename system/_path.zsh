c4a50e61a67567a5aa3062c9a43192fc681d08c9
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2021-04-19 15:56:50 -0500

    4/20/21

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH=./bin:$HOME/bin:$HOME/.yarn/bin:/usr/local/bin:/usr/local/sbin:$HOME/.config/yarn/global/node_modules/.bin:$DOTFILES/bin:$PATH
+export PATH=./bin:$HOME/bin:$HOME/.yarn/bin:/usr/local/bin:/usr/local/sbin:$HOME/.config/yarn/global/node_modules/.bin:$DOTFILES/bin:/Users/bartoszbentkowski/Library/Python/2.7/bin:$PATH
 export MANPATH=/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH
