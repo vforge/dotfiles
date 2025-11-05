44c03c8032679edc5f41607c2875aef1d9718813
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2025-11-05 17:39:15 -0600

    Update PATH and add brew overrides in _path.zsh

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,7 @@
-export PATH=./bin:$HOME/bin:$HOME/.yarn/bin:/usr/local/bin:/usr/local/sbin:$HOME/.config/yarn/global/node_modules/.bin:$DOTFILES/bin:/Users/bartoszbentkowski/Library/Python/2.7/bin:$PATH
+export PATH=./bin:$HOME/bin:$HOME/.yarn/bin:/usr/local/bin:/usr/local/sbin:$HOME/.config/yarn/global/node_modules/.bin:$DOTFILES/bin:~/Library/Python/2.7/bin:~/.local/bin:$PATH
 export MANPATH=/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH
+
+# brew overrides
+if command -v brew >/dev/null 2>&1; then
+  export PATH="$(brew --prefix)/bin:$PATH"
+fi
